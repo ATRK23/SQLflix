ca9c276979c86875bcdcda0955bfce39f2b7f67b
Samy Amine HALIT <halitsamyamine01@yahoo.com>
2024-12-02 18:34:43 +0100

    Create SQLFLIX-createschema.sql

---
+++ SQLFLIX-database/SQLFLIX-createschema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE name (
+    nconst TEXT PRIMARY KEY,
+    primaryName TEXT,
+    birthYear NUMERIC(4,0),
+    deathYear NUMERIC(4,0),
+    primaryProfession TEXT[],           -- Tableau pour les professions 
+    knownForTitles TEXT[]         -- Tableau pour les titres associés
+);
+
+
+CREATE TABLE ratings (
+    tconst TEXT PRIMARY KEY,
+    averageRating NUMERIC(3,1),
+    numVotes INTEGER
+);
+
+CREATE TABLE titlebasics (
+    tconst TEXT PRIMARY KEY,
+    titleType TEXT,
+    primaryTitle TEXT,
+    originalTitle TEXT,
+    isAdult BOOLEAN,
+    startYear NUMERIC(4,0),
+    endYear NUMERIC(4,0),
+    runtimeMinutes INTEGER,
+    genres TEXT[] -- Tableau pour les genres
+);
