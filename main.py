dfbc2a58578ef6d8b06a47e0df5deaf6c04c5c14
Arthur BAILLET <arthur.baillet@edu.univ-paris13.fr>
2024-12-23 23:40:23 +0100

    connection

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
 import os
 import sys
+import psycopg2
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QVBoxLayout, QWidget
 
-def on_button_click():
-    label.setText("Bouton cliqué!")
-    
-app = QApplication(sys.argv)
-
-window = QMainWindow()
-window.setWindowTitle("test")
-window.setGeometry(100, 100, 400, 300)
-
-central_widget = QWidget()
-window.setCentralWidget(central_widget)
-
-layout = QVBoxLayout()
-
-label = QLabel("Bienvenue dans PyQt5")
-layout.addWidget(label)
-
-button = QPushButton("Cliquez ici")
-button.clicked.connect(on_button_click)
-layout.addWidget(button)
-
-central_widget.setLayout(layout)
-
-window.show()
-
-sys.exit(app.exec_())
+def connect_DB(self):
+        self.conn = psycopg2.connect(database="myuniversitydb", user="mynabil", host="localhost", password="test")
+        #self.conn = psycopg2.connect(database="l3info_10", user="l3info_10", host="10.11.11.22", password="L3INFO_10")
+        self.cursor = self.conn.cursor()
